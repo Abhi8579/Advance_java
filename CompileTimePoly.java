4d8a153408c138fe435dc2f40257fafe69107d7d
Ashutosh pandey <shivampandit7985@gmail.com>
2026-02-14 07:33:15 +0530

    Method Overloading

---
+++ CompileTimePoly.java
@@ -0,0 +1,20 @@
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class CompileTimePoly {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+
+        System.out.println("Sum of 2 numbers: " + c.add(10, 20));
+        System.out.println("Sum of 3 numbers: " + c.add(10, 20, 30));
+    }
+}
